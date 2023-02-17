b34253f6541442087bb2c46fd2d5abb348f356c2
Murilo Ribeiro <88835239+MuriloRibg@users.noreply.github.com>
2023-02-17 14:24:13 -0300

    Fast delegate

---
+++ service-http-ecommerce/src/main/java/br/com/alura/ecommerce/GenereteAllReportsServlet.java
@@ -0,0 +1,35 @@
+package br.com.alura.ecommerce;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public class GenereteAllReportsServlet extends HttpServlet {
+    private final KafkaDispatcher<String> batchDispatcher = new KafkaDispatcher<>();
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        batchDispatcher.close();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
+        try {
+
+            batchDispatcher.send("SEND_MASSAGE_TO_ALL_USERS", "USER_GENERATE_READING_REPORT", "USER_GENERATE_READING_REPORT");
+
+            System.out.println("Sent generate report to all users.");
+            resp.setStatus(HttpServletResponse.SC_OK); //Return to consumer
+            resp.getWriter().println("Report requests generated.");
+            
+        } catch (ExecutionException | InterruptedException | IOException e) {
+            throw new ServletException(e);
+        }
+    }
+}
+
---
+++ service-users/src/main/java/br/com/alura/ecommerce/BatchSendMessageService.java
@@ -0,0 +1,64 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class BatchSendMessageService {
+
+    private final Connection connection;
+
+    BatchSendMessageService() throws SQLException {
+        String url = "jdbc:sqlite:service-users/target/users_database.db";
+        this.connection = DriverManager.getConnection(url);
+        try {
+            connection.createStatement().execute("CREATE TABLE USERS (" +
+                    "UUID VARCHAR(200) PRIMARY KEY, " +
+                    "EMAIL VARCHAR(200) NOT NULL)");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        var batchSendMessageService = new BatchSendMessageService();
+        try (var service = new KafkaService<>(
+                BatchSendMessageService.class.getSimpleName(),
+                "SEND_MASSAGE_TO_ALL_USERS",
+                batchSendMessageService::parse,
+                String.class,
+                new HashMap<>())) { //o map é opcional, e por isso está vazio
+            service.run();
+        }
+    }
+
+    private final KafkaDispatcher<User> userKafkaDispatcher = new KafkaDispatcher<>();
+
+    private void parse(ConsumerRecord<String, String> record) throws ExecutionException, InterruptedException, SQLException {
+        System.out.println("-----------------------------------------");
+        System.out.println("Processing new batch");
+        System.out.println("Topic: " + record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+
+        for (User user : getAllUsers()) {
+            userKafkaDispatcher.send(record.value(), user.getUuid(), user);
+        }
+    }
+
+    private List<User> getAllUsers() throws SQLException {
+        var results = connection.prepareStatement("SELECT UUID FROM USERS").executeQuery();
+        List<User> users = new ArrayList<>();
+        while (results.next()) {
+            users.add(new User(results.getString(1)));
+        }
+        return users;
+    }
+}
